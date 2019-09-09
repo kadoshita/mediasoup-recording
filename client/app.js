f1c5d43ba80ebabcd69a864a7f131e6db52ac9eb
kadoshita <sublimer.me@gmail.com>
2019-09-09 15:54:09 +0900

    Remove server check

--- client/app.js
+++ client/app.js
@@ -2,33 +2,39 @@ const puppeteer = require("puppeteer");
 const http = require("http");
 const { exec } = require("child_process");
 
-const startWebpackDevServer = () => {
-    return new Promise((resolve, reject) => {
-        const serverProcess = exec("webpack-dev-server");
-        const serverCheckTimer = setInterval(() => {
-            http.get("http://127.0.0.1:9000/webpack-dev-server/", res => {
-                if (res.statusCode === 200) {
-                    clearInterval(serverCheckTimer);
-                    resolve(serverProcess);
-                }
-            });
-        }, 1000);
+const RETRY_COUNT = 10;
+
+const sleep = msec => {
+    return new Promise(resolve => {
+        setTimeout(resolve, msec);
     });
 };
 
 (async () => {
-    const ps = await startWebpackDevServer();
-
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless:false
+    });
     const page = await browser.newPage();
     await page.goto("http://127.0.0.1:9000/webpack-dev-server/");
 
-    let status = await page.$eval("#status", elm => elm.innerHTML);
-    let okness = await page.$eval("#okness", elm => elm.innerHTML);
+    let status = "";
+    let okness = "";
 
-    console.log(`status: ${status}`);
-    console.log(`okness: ${okness}`);
+    let isReady = false;
+    for(let retry = 0; retry<RETRY_COUNT;retry++){
+        status = await page.$eval("#status", elm => elm.innerHTML);
+        okness = await page.$eval("#okness", elm => elm.innerHTML);
+        console.log(`retry: ${retry} status: ${status} okness: ${okness}`);
+        if(status.toLowerCase()==="app ready."){
+            isReady = true;
+            break;
+        }
+        await sleep(1000);
+    }
+
+    if(!isReady){
+        await browser.close();
+    }
 
     await browser.close();
-    ps.kill();
 })();
